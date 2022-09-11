9d23ae9fed3a7c409150d30f1add8da4a1f27ada
lizac9 <lizacarelli@gmail.com>
2022-09-11 16:36:42 +0000

    Add scores js functionality

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,7 +1,7 @@
 const username = document.querySelector('#username');
 const saveScoreBtn = document.querySelector('#saveScoreBtn');
 const finalScore = document.querySelector('#finalScore');
-const mostRecentScore = document.querySelector('#mostRecentScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
