7caf3f47e11a7854e43582f96958ae4fda5c8ddb
lizac9 <lizacarelli@gmail.com>
2022-09-16 20:25:06 +0000

    Change background image

--- assets/js/end.js
+++ assets/js/end.js
@@ -3,32 +3,32 @@ const saveScoreBtn = document.querySelector('#saveScoreBtn');
 const finalScore = document.querySelector('#finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-const MAX_HIGH_SCORES = 5
+const MAX_HIGH_SCORES = 5;
 
-finalScore.innerText = mostRecentScore
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value
-})
+    saveScoreBtn.disabled = !username.value;
+});
 
 saveHighScore = e => {
-    e.preventDefault()
+    e.preventDefault();
 
     const score = {
         score: mostRecentScore,
         name: username.value
     }
 
-    highScores.push(score)
+    highScores.push(score);
 
     highScores.sort((a, b) => {
-        return b.score - a.score
+        return b.score - a.score;
     })
 
-    highScores.splice(5)
+    highScores.splice(5);
 
-    localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign('index.html')
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('index.html');
 }
